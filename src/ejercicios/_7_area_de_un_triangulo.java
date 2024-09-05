3a22dd3de607bd7ba6efbce47a991da07832eee4
pablo <Pablo.Contreras@tsoftglobal.com>
2024-09-05 15:08:39 -0400

    Ejercicio 7 : Área de un triángulo •	Descripción: Dadas la base b = 5 y la altura h = 8 de un triángulo, calcula su área usando la fórmula: ALTURA = 9/5 X C + 32
    •	Variables: b = 5, h = 8.

---
+++ src/ejercicios/_7_area_de_un_triangulo.java
@@ -0,0 +1,11 @@
+package ejercicios;
+
+public class _7_area_de_un_triangulo {
+            public static void main(String[] args) {
+                double base = 5;
+            double altura = 8;
+            double area;
+            area = (base * altura) / 2;
+            System.out.println("El área del triángulo es: " + area);
+        }
+    }
