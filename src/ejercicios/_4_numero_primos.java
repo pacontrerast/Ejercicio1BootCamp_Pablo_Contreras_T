324564f8dca2070bf39b18d3beae7156e336abf5
pablo <Pablo.Contreras@tsoftglobal.com>
2024-09-05 17:03:30 -0400

    Ejercicio 4 : Números primos •	Descripción: Dado el número a = 29, determina si es primo. Un número primo solo tiene dos divisores: 1 y él mismo. •	Variables: a = 29. (modificación de codigo v2.0)

--- src/ejercicios/_4_numero_primos.java
+++ src/ejercicios/_4_numero_primos.java
@@ -1,25 +1,25 @@
 package ejercicios;
 
 public class _4_numero_primos {
-
-        public static boolean esPrimo(int numero) {
-            if (numero <= 1) {
-                return false;
-            }
-            for (int i = 2; i <= numero / 2; i++) {
-                if (numero % i == 0) {
-                    return false; //
-                }
-            }
-            return true;
-        }
         public static void main(String[] args) {
             int numero = 29;
-            if (esPrimo(numero)) {
+            boolean numeroPrimo = true;
+
+            for (int i = 2; i < numero; i++) {
+                if (numero % i == 0) {
+                    numeroPrimo = false;
+                    break;
+                }
+            }
+
+            if (numeroPrimo) {
                 System.out.println(numero + " es un número primo.");
             } else {
                 System.out.println(numero + " no es un número primo.");
             }
         }
+
+
+
 }
 
