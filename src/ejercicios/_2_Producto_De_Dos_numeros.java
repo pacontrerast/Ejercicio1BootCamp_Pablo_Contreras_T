8d7827f7ed76599e27a7ec0aa9d8bf2b5f5c3c50
pablo <Pablo.Contreras@tsoftglobal.com>
2024-09-05 15:05:51 -0400

    Ejercicio 2 : Producto de dos números •	Descripción: Dados los números a = 7 y b = 8, calcula el producto de ambos y muestra el resultado. •	Variables: a = 7, b = 8.

---
+++ src/ejercicios/_2_Producto_De_Dos_numeros.java
@@ -0,0 +1,12 @@
+package ejercicios;
+
+public class _2_Producto_De_Dos_numeros {
+
+        public static void main(String[] args) {
+            int a = 7;
+            int b = 8;
+            int producto = a * b;
+            System.out.println("El producto de " + a + " y " + b + " es: " + producto);
+        }
+    }
+
