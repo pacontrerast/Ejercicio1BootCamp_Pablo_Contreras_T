6cb435c7bbc056dec279815b921041d114138b33
pablo <Pablo.Contreras@tsoftglobal.com>
2024-09-05 15:07:36 -0400

    Ejercicio 5 : Promedio de tres números •	Descripción: Dados los números a = 4, b = 9 y c = 13, calcula el promedio de los tres. •	Variables: a = 4, b = 9, c = 13.

---
+++ src/ejercicios/_6_Promedio_de_tres_numeros.java
@@ -0,0 +1,17 @@
+package ejercicios;
+
+public class _6_Promedio_de_tres_numeros {
+        public static void main(String[] args) {
+            int a = 4, b = 9, c = 13;
+            double promedio;
+            promedio = (double)(a + b + c) / 3;
+            System.out.println("El promedio de los siguientes numeros");
+            System.out.println("a ="+a);
+            System.out.println("b ="+b);
+            System.out.println("c ="+c);
+            System.out.println("====");
+            System.out.println("El promedio de los 3 numeros es :");
+            System.out.println(promedio);
+
+        }
+    }
