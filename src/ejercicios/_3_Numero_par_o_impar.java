deac158ac210108709ae6df6e34009c40286cbcf
pablo <Pablo.Contreras@tsoftglobal.com>
2024-09-05 15:06:31 -0400

    Ejercicio 3 : Número par o impar •	Descripción: Dado el número a = 15, determina si es par o impar. Un número es par si es divisible por 2, y es impar si no lo es. •	Variables: a = 15.

---
+++ src/ejercicios/_3_Numero_par_o_impar.java
@@ -0,0 +1,13 @@
+package ejercicios;
+
+public class _3_Numero_par_o_impar {
+        public static void main(String[] args) {
+            int a = 15;
+            if (a % 2 == 0) {
+                System.out.println(a + " es un número par.");
+            } else {
+                System.out.println(a + " es un número impar.");
+            }
+        }
+    }
+
