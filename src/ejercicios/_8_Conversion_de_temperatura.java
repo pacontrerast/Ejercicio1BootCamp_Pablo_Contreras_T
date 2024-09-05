87db3c09c832495bc4c231921efbbebf37c71e6c
pablo <Pablo.Contreras@tsoftglobal.com>
2024-09-05 15:09:49 -0400

    Ejercicio 8 : Conversión de temperatura •	Descripción: Dado que la temperatura es c = 25 grados Celsius, conviértela a Fahrenheit usando la fórmula: F=9/5 X C + 32
    •	Variables: c = 25.

---
+++ src/ejercicios/_8_Conversion_de_temperatura.java
@@ -0,0 +1,12 @@
+package ejercicios;
+
+public class _8_Conversion_de_temperatura {
+        public static void main(String[] args) {
+            double celsius = 25;
+            double fahrenheit;
+            fahrenheit = (celsius * 9/5) + 32;
+            System.out.println("Equivalencia entre Celsius y fahrenheit ");
+            System.out.println(celsius + " C° equivalen a " + fahrenheit + " F° ");
+        }
+
+}
