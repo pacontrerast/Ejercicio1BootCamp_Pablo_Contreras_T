067274587494097b9c2274aa41128882bb4917ca
pablo <Pablo.Contreras@tsoftglobal.com>
2024-09-05 15:00:04 -0400

    Ejercicio 1 : Suma de dos números •	Descripción: Dados los números a = 10 y b = 20, calcula la suma de ambos y muestra el resultado. •	Variables: a = 10, b = 20.

---
+++ src/ejercicios/_1_suma_De_Dos_numeros.java
@@ -0,0 +1,12 @@
+package ejercicios;
+
+public class _1_suma_De_Dos_numeros {
+
+        public static void main(String[] args) {
+            int a = 10;
+            int b = 20;
+            int suma = a + b;
+            System.out.println("La suma de " + a + "+" + b + " = " + suma);
+        }
+    }
+
